7792d11fe94be7f60cea990a2007e6cb647ff3a1
Dan Anttila <115313515+D-Antt@users.noreply.github.com>
2023-10-02 17:58:58 -0400

    the commit for week one.  It works at the end of the video.  Mainly the header

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import Header from "./components/Header/Header";
+import { BrowserRouter } from "react-router-dom";
+import ThemeContextProvider from "./contexts/ThemeContext";
+
+function App() {
+  return (
+    <ThemeContextProvider>
+      <BrowserRouter>
+        <Header />
+      </BrowserRouter>
+    </ThemeContextProvider>
+  );
+}
+
+export default App;
